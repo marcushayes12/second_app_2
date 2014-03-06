6010606356feb6e6b3563e8e821b5f82c07971c7
1 <1>
2014-03-06 12:45:26 -0600

    finished second app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 SecondApp2::Application.routes.draw do
+  resources :miniposts
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
